e3184bf902164231281949386d96a1d8054f637f
Said <alta0073@algonquinlive.com>
2025-06-15 15:36:16 -0400

    /ping route added for healthcheck for uptime monitoring

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,14 @@ const cors = require("cors");
 const path = require("path");
 const connectDB = require("./config/db");
 
-const authRoutes = require('./routes/authRoutes')
-const sessionRoutes = require('./routes/sessionRoutes')
-const questionRoutes = require('./routes/questionRoutes');
+const authRoutes = require("./routes/authRoutes");
+const sessionRoutes = require("./routes/sessionRoutes");
+const questionRoutes = require("./routes/questionRoutes");
 const { protect } = require("./middlewares/authMiddleware");
-const { generateInterviewQuestions, generateConceptExplanation } = require("./controllers/aiController");
+const {
+  generateInterviewQuestions,
+  generateConceptExplanation,
+} = require("./controllers/aiController");
 
 const app = express();
 
@@ -21,18 +24,23 @@ app.use(
   })
 );
 
-connectDB()
+connectDB();
 
 // Middleware
 app.use(express.json());
 
 // Routes
 app.use("/api/auth", authRoutes);
-app.use('/api/sessions', sessionRoutes);
-app.use('/api/questions', questionRoutes);
+app.use("/api/sessions", sessionRoutes);
+app.use("/api/questions", questionRoutes);
 
 app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
 app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
+
+// Health check endpoint for uptime monitoring
+app.get("/ping", (req, res) => {
+  res.status(200).send("pong");
+});
 
 // Serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
