598c986861a1873e85fbed16df9991a603b3a8ce
Said <alta0073@algonquinlive.com>
2025-06-16 08:13:10 -0400

    mobile fixes 5

--- frontend/interview-prep-ai/src/components/Modal.jsx
+++ frontend/interview-prep-ai/src/components/Modal.jsx
@@ -1,19 +1,12 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 const Modal = ({ children, isOpen, onClose, title, hideHeader }) => {
   if (!isOpen) return null;
 
-  useEffect(() => {
-    document.body.style.overflow = "hidden";
-    return () => {
-      document.body.style.overflow = "auto";
-    };
-  }, []);
-
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center bg-white/20 backdrop-blur-sm  w-full h-full">
       {/* Modal Content */}
-      <div className="relative flex flex-col bg-[#151322] text-black shadow-2xl shadow-purple-500/30 rounded-xl overflow-y-auto max-h-[90vh]">
+      <div className="relative flex flex-col bg-[#151322] text-black shadow-2xl shadow-purple-500/30 rounded-xl max-h-[80vh]">
         {/* Modal Header */}
         {!hideHeader && (
           <div className="flex items-center justify-between p-4 border-b border-purple-700">
